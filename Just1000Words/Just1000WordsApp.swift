b0c4066dad6d71f99b81ff22a7412e089f47ab2a
Vojtěch Kalivoda <kalivoda167@gmail.com>
2025-06-18 16:24:49 +0200

    Initial Commit

---
+++ Just1000Words/Just1000WordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just1000WordsApp.swift
+//  Just1000Words
+//
+//  Created by Vojtěch Kalivoda on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Just1000WordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Just1000WordsTests/Just1000WordsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Just1000WordsTests.swift
+//  Just1000WordsTests
+//
+//  Created by Vojtěch Kalivoda on 6/18/25.
+//
+
+import Testing
+@testable import Just1000Words
+
+struct Just1000WordsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
